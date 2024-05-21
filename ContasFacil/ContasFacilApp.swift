66da61a668c1039259d5c708aed254b3aebe5ede
Elisson Lima <31016448+elissonlima@users.noreply.github.com>
2024-05-21 19:14:57 -0300

    Initial Commit

---
+++ ContasFacil/ContasFacilApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContasFacilApp.swift
+//  ContasFacil
+//
+//  Created by Elisson Silva on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ContasFacilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ContasFacil/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContasFacil
+//
+//  Created by Elisson Silva on 21/05/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
